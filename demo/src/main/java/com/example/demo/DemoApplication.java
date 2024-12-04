8db602463902200086e393ce4f47742be74e2d8e
Cole4651 <133807007+Cole4651@users.noreply.github.com>
2024-12-04 01:14:58 -0600

    Update DemoApplication.java

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -2,12 +2,14 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling  // Enable scheduled tasks
 public class DemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        System.out.println("Starting application");
+        SpringApplication.run(DemoApplication.class, args);
+    }
 }
